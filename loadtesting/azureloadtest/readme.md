a9c46a1b8f935742e6e749e7357a9cdf40238ffd
Vincent Rouet <vincent.rouet@microsoft.com>
2021-04-09 17:54:28 +0100

    save cnages

--- loadtesting/azureloadtest/readme.md
+++ loadtesting/azureloadtest/readme.md
@@ -5,3 +5,45 @@ PG: John Stallo
 Private preview: Feb 2021
 
 JMeter examples: https://github.com/QASymphony/jmeter-sample/blob/master/Users.jmx
+
+Hey documentation https://github.com/rakyll/hey
+
+## Metrics queries:
+
+### KQL Queries
+
+
+- Function App Instance over Time Query
+```
+let grainTime = 30sec;
+
+traces
+
+| where timestamp >= ago(24h)
+| summarize ['rate/minute'] = dcount(cloud_RoleInstance) by bin(timestamp, grainTime)
+| order by timestamp desc
+| render timechart
+```
+- Function App Instance using Specific Time Interval
+```
+let max_t = <start time here>;
+let min_t = <min time here>;
+requests
+| where timestamp between(min_t..max_t)
+| summarize Count_Function_App_Instances=dcount(cloud_RoleInstance) by bin(timestamp, 30s)
+| render timechart
+```
+- Unique Function Instances
+```
+let grainTime = 30sec;
+traces
+| where timestamp >= ago(24h)
+| distinct cloud_RoleInstance
+```
+- Requests Per Second
+```
+requests
+| where timestamp > ago(1h)
+| summarize totalCount=sum(itemCount) by bin(timestamp, 1s)
+| render timechart
+```
--- loadtesting/hey/funceh.sh
+++ loadtesting/hey/funceh.sh
@@ -11,7 +11,7 @@ docker push virouet/loadtest:v1
 hey -n 10 -c 2 -m GET -T "application/json" https://func-ingestmeasure-prem.azurewebsites.net/api/Function1?code=3PXIkcYozXNIwaR9H9lNx/g6xDExvgxVdyauPdOZIKqHQJMNM2jh7Q==
 
 # Variables
-RG=funcehtest-rg
+RG=demo-rg-serverless-ingestion-acis
 
 # Single Test(s)
 docker run --rm --name loadtest01 virouet/loadtest:v1 -n 10 -c 2 -m GET -T "application/json" https://func-ingestmeasure-prem.azurewebsites.net/api/Function1?code=3PXIkcYozXNIwaR9H9lNx/g6xDExvgxVdyauPdOZIKqHQJMNM2jh7Q==
@@ -27,29 +27,48 @@ az container list -o table
 for value in {1..49}
 do
     #az container create -g $RG --name loadtest$value --image kevingbb/hey:v1 --restart-policy Never --no-wait --command-line "/hey -n 10 -c 2 -m GET -T 'application/json' https://funcehtest01.azurewebsites.net/api/httpgettest?code=a/FB/hXxXtaPclZa5kqL/V0CMXh2Heye85cXjt3max4IawBvcF0gZg==&name=Fred" &
-    az container create -g $RG -l northeurope --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 10 -m GET -T 'application/json' https://func-ingestmeasure-prem.azurewebsites.net/api/Function1?code=3PXIkcYozXNIwaR9H9lNx/g6xDExvgxVdyauPdOZIKqHQJMNM2jh7Q==" > /dev/null &
+    
+    #Premium Function plan
+    az container create -g $RG -l northeurope --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 5m -c 40 -m GET -T 'application/json' https://func-ingestion-premium.azurewebsites.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
+    #App Service Function plan
+    #az container create -g $RG -l northeurope --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://func-ingestion-appservice.azurewebsites.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
+    #Via Azure Front Door
+    # az container create -g $RG -l northeurope --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://serverlessingest.azurefd.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
 done
+
 # Another Location
-for value in {50..99}
+for value in {101..149}
 do
     #az container create -g $RG --name loadtest$value --image kevingbb/hey:v1 --restart-policy Never --no-wait --command-line "/hey -n 10 -c 2 -m GET -T 'application/json' https://funcehtest01.azurewebsites.net/api/httpgettest?code=a/FB/hXxXtaPclZa5kqL/V0CMXh2Heye85cXjt3max4IawBvcF0gZg==&name=Fred" &
-    az container create -g $RG -l uksouth --name loadtest$value --image kevingbb/hey:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://funcehtest001.azurewebsites.net/api/httpgettest?code=inwRPvFYFSOj2468p7lHEMIKaJ/EG8NEHkLqQ2XsDAB74fwA2voVVg==&name=${value}" > /dev/null &
+
+    #Premium plan
+    #az container create -g $RG -l uksouth --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://func-ingestion-premium.azurewebsites.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
+    #App Service Function plan
+    #az container create -g $RG -l uksouth --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://func-ingestion-appservice.azurewebsites.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
+    #Via Azure Front Door
+    az container create -g $RG -l uksouth --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://serverlessingest.azurefd.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
+
 done
 # Another Location
-for value in {200..299}
+for value in {100..149}
 do
     #az container create -g $RG --name loadtest$value --image kevingbb/hey:v1 --restart-policy Never --no-wait --command-line "/hey -n 10 -c 2 -m GET -T 'application/json' https://funcehtest01.azurewebsites.net/api/httpgettest?code=a/FB/hXxXtaPclZa5kqL/V0CMXh2Heye85cXjt3max4IawBvcF0gZg==&name=Fred" &
-    az container create -g $RG -l francecentral --name loadtest$value --image kevingbb/hey:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://funcehtest001.azurewebsites.net/api/httpgettest?code=inwRPvFYFSOj2468p7lHEMIKaJ/EG8NEHkLqQ2XsDAB74fwA2voVVg==&name=${value}" > /dev/null &
+    az container create -g $RG -l francecentral --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://func-ingestion-premium.azurewebsites.net/api/measure_session?code=tBW3ag6lmc6BYDYeiozQhbGYrlDpgZVDe1BZtMJh7vaH62J1uoMtpw==&name=${value}" > /dev/null &
 done
 # Another Location
 for value in {300..399}
 do
     #az container create -g $RG --name loadtest$value --image kevingbb/hey:v1 --restart-policy Never --no-wait --command-line "/hey -n 10 -c 2 -m GET -T 'application/json' https://funcehtest01.azurewebsites.net/api/httpgettest?code=a/FB/hXxXtaPclZa5kqL/V0CMXh2Heye85cXjt3max4IawBvcF0gZg==&name=Fred" &
-    az container create -g $RG -l westeurope --name loadtest$value --image kevingbb/hey:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://funcehtest001.azurewebsites.net/api/httpgettest?code=inwRPvFYFSOj2468p7lHEMIKaJ/EG8NEHkLqQ2XsDAB74fwA2voVVg==&name=${value}" > /dev/null &
+    az container create -g $RG -l westeurope --name loadtest$value --image virouet/loadtest:v1 --cpu 4 --memory 8 --restart-policy Never --no-wait --command-line "/hey -z 10m -c 40 -m GET -T 'application/json' https://func-ingestion-premium.azurewebsites.net/api/HttpMeasure?code=3EDPSyJEbP9BD282M4g6AomzwFxmE4OyCWQvs5jYTcflBnrY8Lr0wg==&name=${value}" > /dev/null &
 done
 
 # Use For Loop for Deleting
-for value in {1..99}
+for value in {1..49}
 do
     az container delete -g $RG --name loadtest$value -y > /dev/null &
 done
